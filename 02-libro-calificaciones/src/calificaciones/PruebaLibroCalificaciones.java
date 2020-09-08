e582c250846660432c6d5adf82f47662605bec05
Brian Romano <brianromanook@gmail.com>
2020-09-07 23:12:42 -0300

    Ejecicios POO en proceso

--- 02-libro-calificaciones/src/calificaciones/PruebaLibroCalificaciones.java
+++ 02-libro-calificaciones/src/calificaciones/PruebaLibroCalificaciones.java
@@ -2,8 +2,14 @@ package calificaciones;
 
 public class PruebaLibroCalificaciones {
     public static void main(String[] args) {
+        //Objetos
         LibroCalificaciones calificaciones = new LibroCalificaciones();
-        calificaciones.setNombreCurso("1ero A");
+        LibroCalificaciones calificaciones1 = new LibroCalificaciones();
+        //Set
+        calificaciones.setNombreCurso("POO");
+        calificaciones1.setNombreCurso("Bases de datos");
+        //Get
         System.out.println("Nombre curso: "+calificaciones.getNombreCurso());
+        System.out.println("Nombre curso: "+calificaciones1.getNombreCurso());
     }
 }
