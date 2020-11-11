e88c5bbcb57c0ea0c46c4e46ce9e549c0a2aacad
Brian Romano <brianromanook@gmail.com>
2020-11-11 18:28:20 -0300

    Ejercicio 4 finalizado

---
+++ 16-constructores-encapsulamiento/src/dateFour/DateFour.java
@@ -0,0 +1,16 @@
+package dateFour;
+
+public class DateFour {
+    private int day;
+    private int month;
+    private int year;
+
+    //Constructor por defecto
+    public DateFour(){}
+    //Constructor sobrecargado
+    public DateFour(int day, int month, int year){
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+}
---
+++ 16-constructores-encapsulamiento/src/dateFour/DateFourTest.java
@@ -0,0 +1,7 @@
+package dateFour;
+
+public class DateFourTest {
+    public static void main(String[] args) {
+        DateFour fecha = new DateFour(18, 11, 2020);
+    }
+}
