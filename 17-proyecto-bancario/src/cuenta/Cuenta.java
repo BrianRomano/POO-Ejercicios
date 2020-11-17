4c66b9b1a66e2661ad7aca6ec90bb9c47903290d
Brian Romano <brianromanook@gmail.com>
2020-11-16 21:57:10 -0300

    Identación

--- 17-proyecto-bancario/src/cuenta/Cuenta.java
+++ 17-proyecto-bancario/src/cuenta/Cuenta.java
@@ -3,17 +3,20 @@ package cuenta;
 public class Cuenta {
     private double balance;
 
-    public Cuenta(double balance){
+    public Cuenta(double balance) {
         this.balance = balance;
     }
+
     public double getBalance() {
         return balance;
     }
-    public void depositar(double monto){
+
+    public void depositar(double monto) {
         balance += monto;
     }
+
     public void retirar(double monto){
-        if(monto <= balance){
+        if (monto <= balance) {
             balance -= monto;
         } else {
             System.out.println("No dispone de esa cantidad de dinero");
